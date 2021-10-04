b109ad09797290a28852cce4febe90c41ef1ea8f
Danfeng <89091014+danfenglu@users.noreply.github.com>
2021-10-03 23:49:10 -0400

    Add files via upload

---
+++ web115/scripts/fizzbuzz1.js
@@ -0,0 +1,48 @@
+function getData() {
+    let firstName = document.getElementById('first_name').value;
+    let lastName = document.getElementById('last_name').value;
+    let middleInitial = document.getElementById('middle_initial').value;
+          
+    if (middleInitial) {
+       document.getElementById("greeting").textContent = "Welcome to Kyko Kombucha, " + firstName + " " + middleInitial + ". " + lastName + "!";
+     }
+    
+    club();
+  }
+  
+  function club() {
+    for (let i = 1; i <= 140; i++) {
+      var word = '';
+      
+      if (i % 3 === 0) {
+        word = word.concat('Lesson A');
+      } 
+      
+      if (i % 5 === 0) {
+        word = word.concat('Lesson B');
+      }
+      
+      if (word === '') {
+        word = 'Lesson x';
+      }
+        
+        
+      
+      //creates new element & stores in variable
+      var newListItem = document.createElement('li');
+      
+      //creates text node & stores in variable
+      var newText = document.createTextNode(i + "- " + word);
+      
+      //attaches text node to new element
+      newListItem.appendChild(newText);
+      
+      //find position where element should be added
+      var position = document.getElementsByTagName('ul')[0];
+      
+      //insert new element in correct position
+      position.appendChild(newListItem);
+      
+    }
+    
+  }
