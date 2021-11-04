2c22bab53adabcb32806e73e8dcb1cafa3107c8a
Danfeng <89091014+danfenglu@users.noreply.github.com>
2021-11-03 23:10:03 -0400

    Add files via upload

--- web115/scripts/fizzbuzz0.js
+++ web115/scripts/fizzbuzz0.js
@@ -1,44 +1,30 @@
-
-function getData() {
-  let firstName = document.getElementById('first_name').value;
-  let lastName = document.getElementById('last_name').value;
-  let middleInitial = document.getElementById('middle_initial').value;
-        
-  if (middleInitial) {
-     document.getElementById("greeting").textContent = "Welcome to Language Learning Club, " + firstName + " " + middleInitial + ". " + lastName + "!";
-   } else {
-     document.getElementById("greeting").textContent = "Welcome to Language Learning Club, " + firstName + " " + lastName + "!";
-   }
-  
-  let howHigh = prompt(`How high do you want to count, ${firstName}?`);
-  lesson(howHigh);
-}
-
-function lesson(counter) {
-  for(let i = 0; i <= counter; i++) {
-    var evenOrOdd;
-    
-    if (i % 2 === 0) {
-      evenOrOdd = 'the number is even';
-    } else {
-      evenOrOdd = 'the number is odd';
-    }
-    
-    //creates new element & stores in variable
-    var newListItem = document.createElement('li');
-    
-    //creates text node & stores in variable
-    var newText = document.createTextNode(i + '- learning club - ' + evenOrOdd);
-    
-    //attaches text node to new element
-    newListItem.appendChild(newText);
-    
-    //find position where element should be added
-    var position = document.getElementsByTagName('ul')[0];
-    
-    //insert new element in correct position
-    position.appendChild(newListItem);
-    
-  }
-  
+function changeGreeting2() {					// This function personalizes the h3 greeting for the page
+	var first = document.getElementById("first_name").value;
+	var mid   = document.getElementById("middle_initial").value;	// Get user's name
+	var last  = document.getElementById("last_name").value;
+			
+	// Write out welcome using user input
+	if (mid !== "") {
+		document.getElementById("greeting").innerHTML = "Welcome to Learning Language Club, " + first + " " + mid + ". " + last + "!";
+	} else {
+		document.getElementById("greeting").innerHTML = "Welcome to Learning Language Club, " + first + " " + last + "!";		
+	}
+			
+	// Prompt user for a number
+	var howHigh = prompt("How high do you want to count, " + first + "?");
+	var thisHigh = parseInt(howHigh);
+		
+	// Create loop which prints different message based on whether user's input was even or odd.
+	var msg = "";
+	var i;
+	for (i = 1; i <= thisHigh; i++) {
+		if (i % 2 == 0) {
+			msg += i + ".&nbsp;The number is even." + "<br>";
+		} else {
+			msg += i + ".&nbsp;The number is odd." + "<br>";
+		}
+	// Write output to page
+	var writeToPage = document.getElementById("loop");
+	writeToPage.innerHTML = msg;
+	}
 }
