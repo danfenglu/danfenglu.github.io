39a16a89234c43f7eb322f05a4be638bb6ae486d
Danfeng <89091014+danfenglu@users.noreply.github.com>
2021-12-07 06:31:00 -0500

    Update introForm.js

--- web115/scripts/introForm.js
+++ web115/scripts/introForm.js
@@ -5,12 +5,14 @@ function introForm() {
 	var cp = document.getElementById("computer_platform").value;
 	var perBack = document.getElementById("personal").value;
 	var acaBack = document.getElementById("academic").value;
+	var courses= document.getElementById("courses").value;
 	var share = document.getElementById("share").value;
 	
 	document.getElementById("name").innerHTML = "Name: " + firstname + " " + lastname;	
 	document.getElementById("platform").innerHTML = "Primary Computer Platform: " + cp;
 	document.getElementById("personalBackground").innerHTML = "Personal Background: " + perBack;
 	document.getElementById("academicBackground").innerHTML = "Academic Background: " + acaBack;
+	document.getElementById("courses").innerHTML = "Your courses and why you're taking them: <br>" + courses;
 	document.getElementById("Share").innerHTML = "I'd Also Like to Share: " + share;
 
 }
