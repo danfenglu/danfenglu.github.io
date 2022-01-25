b19f698896e6a7d08f885e0c8f10f143a2cbafe6
Danfeng <89091014+danfenglu@users.noreply.github.com>
2022-01-25 13:53:31 -0500

    Add files via upload

---
+++ web250/scripts/introForm.js
@@ -0,0 +1,18 @@
+function introForm() {
+	
+	var firstname = document.getElementById("first_name").value;
+	var lastname = document.getElementById("last_name").value;
+	var cp = document.getElementById("computer_platform").value;
+	var perBack = document.getElementById("personal").value;
+	var acaBack = document.getElementById("academic").value;
+	var course= document.getElementById("takingcourse").value;
+	var share = document.getElementById("share").value;
+	
+	document.getElementById("name").innerHTML = "Name: " + firstname + " " + lastname;	
+	document.getElementById("platform").innerHTML = "Primary Computer Platform: " + cp;
+	document.getElementById("personalBackground").innerHTML = "Personal Background: " + perBack;
+	document.getElementById("academicBackground").innerHTML = "Academic Background: " + acaBack;
+	document.getElementById("mycourse").innerHTML = "Your courses and why you're taking them: <br>" + course;
+	document.getElementById("Share").innerHTML = "I'd Also Like to Share: " + share;
+
+}
