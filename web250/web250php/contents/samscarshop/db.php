029c96baa78b6e7aa4abe040ee200a0c5eb44e09
Danfeng <89091014+danfenglu@users.noreply.github.com>
2022-03-30 11:40:31 -0400

    Update db.php

--- web250/web250php/contents/samscarshop/db.php
+++ web250/web250php/contents/samscarshop/db.php
@@ -1,5 +1,5 @@
  <?php
-$mysqli = new mysqli('mySQL', 'root', 'verysecret', 'Cars' );
+$mysqli = new mysqli('	sql102.epizy.com', 'epiz_31064065', 'vMcSwU5CGXos', 'vMcSwU5CGXos_cars' );
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
